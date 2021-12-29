2c53e5e2297274ee0790b4179ca7280e4f9f404a
Stephan Kulla <git.mail@kulla.me>
2021-12-29 21:10:43 +0100

    tests: Use assert-feature of pytest

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,12 +2,11 @@ from unittest import TestCase
 
 from serlo.utils import get_domain, get_subdomain
 
-class TestUtils(TestCase):
-    def test_get_domain(self):
-        self.assertEqual(get_domain("https://de.serlo.org/"), "serlo.org")
-        self.assertEqual(get_domain("https://serlo.org/"), "serlo.org")
-        self.assertEqual(get_domain("https://org/"), "org")
+def test_get_domain():
+    assert get_domain("https://de.serlo.org/") == "serlo.org"
+    assert get_domain("https://serlo.org/") == "serlo.org"
+    assert get_domain("https://org/") == "org"
 
-    def test_get_subdomain(self):
-        self.assertEqual(get_subdomain("https://de.serlo.org/"), "de")
-        self.assertEqual(get_subdomain("https://serlo.org/"), "")
+def test_get_subdomain():
+    assert get_subdomain("https://de.serlo.org/") == "de"
+    assert get_subdomain("https://serlo.org/") == ""
