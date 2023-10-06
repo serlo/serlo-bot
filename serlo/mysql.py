95afce4043d09c67c2bd49f917aac972976370a3
Hugo Tiburtino <hugotiburtino@protonmail.com>
2023-10-07 00:45:52 +0200

    chore(mysql): fix password and pull policy of container

--- serlo/mysql.py
+++ serlo/mysql.py
@@ -3,7 +3,7 @@ import mysql.connector
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="password",
+    password="secret",
     port="3306",
     database="serlo",
     charset="latin1"
